e05b13a7d69f741d4d23d07f0c28b07722d02e0d
elong0527 <elong0527@gmail.com>
2021-11-25 02:51:05 +0000

    Deploying to gh-pages from @ keaven/gsd-deming@ed207763d3e2986c5e72c60c5baa7b82d6da836b 🚀

--- slides/render.R
+++
@@ -1,8 +0,0 @@
-library(magrittr); 
-"slides/gsd-deming-slides.Rmd" %>% rmarkdown::render() %>% browseURL()
-
-# "slides/RinPharma2021KeavenAnderson.Rmd" %>% rmarkdown::render() %>% browseURL()
-
-"vignettes/CureModelDesign.Rmd" %>% rmarkdown::render() %>% browseURL()
-
-"vignettes/EXAMINEdesignupdatereport.Rmd" %>% rmarkdown::render() %>% browseURL()
